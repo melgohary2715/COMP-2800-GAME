c79c6acecfa5b3399a0fa3cf99f73c14dd6f93a1
melgohary2715 <melgohary2715@gmail.com>
2024-04-06 18:48:04 -0400

    game container

--- src/objects/GameContainer.java
+++ src/objects/GameContainer.java
@@ -6,30 +6,35 @@ import main.Game;
 
 public class GameContainer extends GameObject {
 
-	public GameContainer(int x, int y, int objType) {
-		super(x, y, objType);
-		createHitbox();
-	}
+    // Constructor for GameContainer
+    public GameContainer(int x, int y, int objType) {
+        super(x, y, objType);
+        createHitbox(); // Create the hitbox for the container
+    }
 
-	private void createHitbox() {
-		if (objType == BOX) {
-			initHitbox(25, 18);
+    // Creates the hitbox for the container based on its type
+    private void createHitbox() {
+        if (objType == BOX) {
+            initHitbox(25, 18); // Initialize hitbox for box
 
-			xDrawOffset = (int) (7 * Game.SCALE);
-			yDrawOffset = (int) (12 * Game.SCALE);
+            // Set draw offsets for box
+            xDrawOffset = (int) (7 * Game.SCALE);
+            yDrawOffset = (int) (12 * Game.SCALE);
+        } else {
+            initHitbox(23, 25); // Initialize hitbox for other container types
+            // Set draw offsets for other container types
+            xDrawOffset = (int) (8 * Game.SCALE);
+            yDrawOffset = (int) (5 * Game.SCALE);
+        }
 
-		} else {
-			initHitbox(23, 25);
-			xDrawOffset = (int) (8 * Game.SCALE);
-			yDrawOffset = (int) (5 * Game.SCALE);
-		}
+        // Adjust hitbox position based on draw offsets
+        hitbox.y += yDrawOffset + (int) (Game.SCALE * 2);
+        hitbox.x += xDrawOffset / 2;
+    }
 
-		hitbox.y += yDrawOffset + (int) (Game.SCALE * 2);
-		hitbox.x += xDrawOffset / 2;
-	}
-
-	public void update() {
-		if (doAnimation)
-			updateAnimationTick();
-	}
+    // Updates the container, including its animation if applicable
+    public void update() {
+        if (doAnimation)
+            updateAnimationTick(); // Update animation tick if animation is enabled
+    }
 }
