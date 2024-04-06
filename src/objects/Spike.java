ab903616bd66172e4f9b32e62e4624df987439c6
melgohary2715 <melgohary2715@gmail.com>
2024-04-06 18:55:43 -0400

    spikes and traps

--- src/objects/Spike.java
+++ src/objects/Spike.java
@@ -2,13 +2,18 @@ package objects;
 
 import main.Game;
 
-public class Spike extends GameObject{
+public class Spike extends GameObject {
 
-	public Spike(int x, int y, int objType) {
-		super(x, y, objType);
-		initHitbox(32, 16);
-		xDrawOffset = 0;
-		yDrawOffset = (int)(Game.SCALE * 16);
-		hitbox.y += yDrawOffset;
-	}
+    // Constructor for Spike
+    public Spike(int x, int y, int objType) {
+        super(x, y, objType); // Call the constructor of the parent class
+        initHitbox(32, 16); // Initialize the hitbox for the spike
+
+        // Set draw offsets
+        xDrawOffset = 0;
+        yDrawOffset = (int)(Game.SCALE * 16);
+
+        // Adjust the hitbox position based on the y-draw offset
+        hitbox.y += yDrawOffset;
+    }
 }
