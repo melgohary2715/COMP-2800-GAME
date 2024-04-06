1ee3466ecf409cee4a8096bcc0808226419e435f
melgohary2715 <melgohary2715@gmail.com>
2024-04-06 19:08:13 -0400

    the buttons

--- src/ui/UrmButton.java
+++ src/ui/UrmButton.java
@@ -2,61 +2,67 @@ package ui;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-
 import utilz.LoadSave;
 import static utilz.Constants.UI.URMButtons.*;
 
 public class UrmButton extends PauseButton {
-	private BufferedImage[] imgs;
-	private int rowIndex, index;
-	private boolean mouseOver, mousePressed;
+    private BufferedImage[] imgs; // Array to hold the button images
+    private int rowIndex; // The row index of the button image in the sprite sheet
+    private int index; // Index to select the correct button image based on its state
+    private boolean mouseOver; // Flag to check if the mouse is over the button
+    private boolean mousePressed; // Flag to check if the button is pressed
 
-	public UrmButton(int x, int y, int width, int height, int rowIndex) {
-		super(x, y, width, height);
-		this.rowIndex = rowIndex;
-		loadImgs();
-	}
+    // Constructor
+    public UrmButton(int x, int y, int width, int height, int rowIndex) {
+        super(x, y, width, height);
+        this.rowIndex = rowIndex;
+        loadImgs();
+    }
 
-	private void loadImgs() {
-		BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.URM_BUTTONS);
-		imgs = new BufferedImage[3];
-		for (int i = 0; i < imgs.length; i++)
-			imgs[i] = temp.getSubimage(i * URM_DEFAULT_SIZE, rowIndex * URM_DEFAULT_SIZE, URM_DEFAULT_SIZE, URM_DEFAULT_SIZE);
+    // Method to load button images from the sprite sheet
+    private void loadImgs() {
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.URM_BUTTONS);
+        imgs = new BufferedImage[3]; // Assuming there are 3 images for each button state
+        for (int i = 0; i < imgs.length; i++) {
+            imgs[i] = temp.getSubimage(i * URM_DEFAULT_SIZE, rowIndex * URM_DEFAULT_SIZE, URM_DEFAULT_SIZE, URM_DEFAULT_SIZE);
+        }
+    }
 
-	}
+    // Method to update the button's state
+    public void update() {
+        index = 0; // Default state
+        if (mouseOver) index = 1; // Mouse over state
+        if (mousePressed) index = 2; // Mouse pressed state
+    }
 
-	public void update() {
-		index = 0;
-		if (mouseOver)
-			index = 1;
-		if (mousePressed)
-			index = 2;
+    // Method to draw the button
+    public void draw(Graphics g) {
+        g.drawImage(imgs[index], x, y, URM_SIZE, URM_SIZE, null);
+    }
 
-	}
+    // Method to reset the button's state
+    public void resetBools() {
+        mouseOver = false;
+        mousePressed = false;
+    }
 
-	public void draw(Graphics g) {
-		g.drawImage(imgs[index], x, y, URM_SIZE, URM_SIZE, null);
-	}
+    // Getter for mouseOver
+    public boolean isMouseOver() {
+        return mouseOver;
+    }
 
-	public void resetBools() {
-		mouseOver = false;
-		mousePressed = false;
-	}
+    // Setter for mouseOver
+    public void setMouseOver(boolean mouseOver) {
+        this.mouseOver = mouseOver;
+    }
 
-	public boolean isMouseOver() {
-		return mouseOver;
-	}
+    // Getter for mousePressed
+    public boolean isMousePressed() {
+        return mousePressed;
+    }
 
-	public void setMouseOver(boolean mouseOver) {
-		this.mouseOver = mouseOver;
-	}
-
-	public boolean isMousePressed() {
-		return mousePressed;
-	}
-
-	public void setMousePressed(boolean mousePressed) {
-		this.mousePressed = mousePressed;
-	}
-
+    // Setter for mousePressed
+    public void setMousePressed(boolean mousePressed) {
+        this.mousePressed = mousePressed;
+    }
 }
