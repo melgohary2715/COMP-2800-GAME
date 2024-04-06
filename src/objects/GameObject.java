abd245bc3a60fd82c4d4ea82f67d49e35c37373a
melgohary2715 <melgohary2715@gmail.com>
2024-04-06 18:50:08 -0400

    game object

--- src/objects/GameObject.java
+++ src/objects/GameObject.java
@@ -10,90 +10,93 @@ import java.awt.geom.Rectangle2D;
 import main.Game;
 
 public class GameObject {
+    // Attributes for the game object
+    protected int x, y, objType;
+    protected Rectangle2D.Float hitbox;
+    protected boolean doAnimation, active = true;
+    protected int aniTick, aniIndex;
+    protected int xDrawOffset, yDrawOffset;
 
-	protected int x, y, objType;
-	protected Rectangle2D.Float hitbox;
-	protected boolean doAnimation, active = true;
-	protected int aniTick, aniIndex;
-	protected int xDrawOffset, yDrawOffset;
+    // Constructor for GameObject
+    public GameObject(int x, int y, int objType) {
+        this.x = x;
+        this.y = y;
+        this.objType = objType;
+    }
 
-	public GameObject(int x, int y, int objType) {
-		this.x = x;
-		this.y = y;
-		this.objType = objType;
-	}
+    // Updates the animation tick for the object
+    protected void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= ANI_SPEED) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= GetSpriteAmount(objType)) {
+                aniIndex = 0;
+                if (objType == BOX) {
+                    doAnimation = false;
+                    active = false;
+                }
+            }
+        }
+    }
 
-	protected void updateAnimationTick() {
-		aniTick++;
-		if (aniTick >= ANI_SPEED) {
-			aniTick = 0;
-			aniIndex++;
-			if (aniIndex >= GetSpriteAmount(objType)) {
-				aniIndex = 0;
-				if (objType == BOX) {
-					doAnimation = false;
-					active = false;
-				} 
-				// else if (objType == CANNON_LEFT || objType == CANNON_RIGHT)
-				// 	doAnimation = false;
-			}
-		}
-	}
+    // Resets the object's animation and activation status
+    public void reset() {
+        aniIndex = 0;
+        aniTick = 0;
+        active = true;
 
-	public void reset() {
-		aniIndex = 0;
-		aniTick = 0;
-		active = true;
+        if (objType == BOX)
+            doAnimation = false;
+        else
+            doAnimation = true;
+    }
 
-		if (objType == BOX)
-			doAnimation = false;
-		else
-			doAnimation = true;
-	}
+    // Initializes the hitbox for the object
+    protected void initHitbox(int width, int height) {
+        hitbox = new Rectangle2D.Float(x, y, (int) (width * Game.SCALE), (int) (height * Game.SCALE));
+    }
 
-	protected void initHitbox(int width, int height) {
-		hitbox = new Rectangle2D.Float(x, y, (int) (width * Game.SCALE), (int) (height * Game.SCALE));
-	}
+    // Draws the hitbox for debugging purposes
+    public void drawHitbox(Graphics g, int xLvlOffset) {
+        g.setColor(Color.PINK);
+        g.drawRect((int) hitbox.x - xLvlOffset, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
+    }
 
-	public void drawHitbox(Graphics g, int xLvlOffset) {
-		g.setColor(Color.PINK);
-		g.drawRect((int) hitbox.x - xLvlOffset, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
-	}
+    // Getters and setters for various attributes
+    public int getObjType() {
+        return objType;
+    }
 
-	public int getObjType() {
-		return objType;
-	}
+    public Rectangle2D.Float getHitbox() {
+        return hitbox;
+    }
 
-	public Rectangle2D.Float getHitbox() {
-		return hitbox;
-	}
+    public boolean isActive() {
+        return active;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    public void setAnimation(boolean doAnimation) {
+        this.doAnimation = doAnimation;
+    }
 
-	public void setAnimation(boolean doAnimation) {
-		this.doAnimation = doAnimation;
-	}
+    public int getxDrawOffset() {
+        return xDrawOffset;
+    }
 
-	public int getxDrawOffset() {
-		return xDrawOffset;
-	}
+    public int getyDrawOffset() {
+        return yDrawOffset;
+    }
 
-	public int getyDrawOffset() {
-		return yDrawOffset;
-	}
+    public int getAniIndex() {
+        return aniIndex;
+    }
 
-	public int getAniIndex() {
-		return aniIndex;
-	}
-
-	public int getAniTick() {
-		return aniTick;
-	}
-
+    public int getAniTick() {
+        return aniTick;
+    }
 }
