e636d476ce55c8063203b5ef77bdc7c65482ed34
melgohary2715 <melgohary2715@gmail.com>
2024-04-06 18:30:21 -0400

    state

--- src/gamestates/State.java
+++ src/gamestates/State.java
@@ -7,29 +7,32 @@ import main.Game;
 import ui.MenuButton;
 
 public class State {
+    // Attribute to hold the game instance
+    protected Game game;
 
-	protected Game game;
+    // Constructor for State
+    public State(Game game) {
+        this.game = game;
+    }
 
-	public State(Game game) {
-		this.game = game;
-	}
+    // Method to check if a mouse event is within the bounds of a MenuButton
+    public boolean isIn(MouseEvent e, MenuButton mb) {
+        return mb.getBounds().contains(e.getX(), e.getY());
+    }
 
-	public boolean isIn(MouseEvent e, MenuButton mb) {
-		return mb.getBounds().contains(e.getX(), e.getY());
-	}
+    // Getter for the game instance
+    public Game getGame() {
+        return game;
+    }
 
-	public Game getGame() {
-		return game;
-	}
+    // Method to set the game state and play the corresponding audio
+    @SuppressWarnings("incomplete-switch")
+    public void setGamestate(Gamestate state) {
+        switch (state) {
+            case MENU -> game.getAudioPlayer().playSong(AudioPlayer.MENU_1);
+            case PLAYING -> game.getAudioPlayer().setLevelSong(game.getPlaying().getLevelManager().getLevelIndex());
+        }
 
-	@SuppressWarnings("incomplete-switch")
-	public void setGamestate(Gamestate state) {
-		switch (state) {
-		case MENU -> game.getAudioPlayer().playSong(AudioPlayer.MENU_1);
-		case PLAYING -> game.getAudioPlayer().setLevelSong(game.getPlaying().getLevelManager().getLevelIndex());
-		}
-
-		Gamestate.state = state;
-	}
-
+        Gamestate.state = state;
+    }
 }
